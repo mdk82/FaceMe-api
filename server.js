9a8f2b4045c5e5f9cae5510484f7f93ab0e016a3
Michael Klingelberg <Mike@Michaels-MacBook-Air.local>
2019-01-17 10:43:22 -0800

    adding db to heroku

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'Mike',
-        password : '',
-        database : 'faceme-db'
+        host : process.env.DATABASE_URL,
+        ssl : true
     }
 });
 
